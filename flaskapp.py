022cb640dbf77d981ced6e5f48aa2c6b8f32d358
Noelle Burke <noelle.burke@drake.edu>
2026-04-08 18:37:33 -0500

    update route

--- flaskapp.py
+++ flaskapp.py
@@ -90,9 +90,50 @@ def update_user():
 @app.route('/update-user/<int:user_id>', methods=['GET', 'POST'])
 def update_playlist(user_id):
     user= execute_query(f"SELECT * FROM `User` WHERE user_id = {user_id};")[0]
+    if request.method == 'POST':
+        action = request.form.get('action')
+        song_id = request.form.get('song_id')
+        conn = get_conn()
+        cursor = conn.cursor()
+    if action == 'add': #add here
+       
+        cursor.execute("""
+            INSERT INTO PlaylistSong (playlist_id, song_id, position)
+            SELECT playlist_id, %s, IFNULL(MAX(position)+1,1)
+            FROM Playlist
+            WHERE user_id=%s
+            """, (song_id, user_id))
+        
+    elif action == 'delete': #delete here
+        cursor.execute("""
+            DELETE FROM PlaylistSong
+            WHERE playlist_id = (SELECT playlist_id FROM Playlist WHERE user_id=%s)
+            AND song_id = %s
+            """, (user_id, song_id))
+        
+        conn.commit()
+        cursor.close()
+        conn.close()
+#update here 
+        playlist_rows = execute_query("""
+        SELECT Song.song_id, Song.title, Artist.name AS artist
+        FROM Playlist
+        JOIN PlaylistSong ON Playlist.playlist_id = PlaylistSong.playlist_id
+        JOIN Song ON PlaylistSong.song_id = Song.song_id
+        JOIN Artist ON Song.artist_id = Artist.artist_id
+        WHERE Playlist.user_id = %s
+        ORDER BY PlaylistSong.position
+    """, (user_id,))
+    playlist = [{'song_id': r['song_id'], 'title': r['title'], 'artist': r['artist']} for r in playlist_rows]
+#show all here
+    all_songs = execute_query("""
+        SELECT Song.song_id, Song.title, Artist.name AS artist
+        FROM Song
+        JOIN Artist ON Song.artist_id = Artist.artist_id
+        ORDER BY Song.title
+    """)
 
-
-    return render_template('edit_user.html', user=user)
+    return render_template('edit_user.html', user=user, playlist=playlist, all_songs=all_songs)
 
 # these two lines of code should always be the last in the file
 if __name__ == '__main__':
